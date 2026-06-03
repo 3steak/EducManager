e3b037c22c29bc84a74e4155e45286ceb5790f91
Cyp <98160646+3steak@users.noreply.github.com>
2026-06-03 15:40:44 +0200

    chore: setup backend architecture

---
+++ backend/src/main/java/com/educmanager/EducManagerApplication.java
@@ -0,0 +1,9 @@
+package com.educmanager;
+
+@SpringBootApplication
+public class EducManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducManagerApplication.class, args);
+    }
+}
