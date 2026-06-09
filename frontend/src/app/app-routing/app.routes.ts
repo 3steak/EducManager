aff183e86f4fba63fbef6332d831ab516bb5d79d
Cyp <98160646+3steak@users.noreply.github.com>
2026-06-09 15:12:38 +0200

    chore : add filiere route

--- frontend/src/app/app-routing/app.routes.ts
+++ frontend/src/app/app-routing/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { authGuard } from '../core/auth/auth.guard';
 import { guestGuard } from '../core/auth/guest.guard';
 import { LoginComponent } from '../features/auth/pages/login/login.component';
+import { FiliereListComponent } from '../features/filieres/pages/filiere-list/filiere-list.component';
 import { HomeComponent } from '../features/home/home.component';
 import { MainLayoutComponent } from '../shared/components/main-layout/main-layout.component';
 
@@ -18,6 +19,7 @@ export const routes: Routes = [
     canActivate: [authGuard],
     children: [
       { path: '', component: HomeComponent },
+      { path: 'filieres', component: FiliereListComponent },
     ],
   },
   { path: '**', redirectTo: 'login' },
